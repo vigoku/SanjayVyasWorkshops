10f204cfb6e6c475ce95b2b063aa012cb2b4e85e
Vinayak Gokuldas Kudva <vinayak.kudva@gmail.com>
2023-02-21 23:16:44 +0530

    rv

---
+++ RV/vanilla/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.querySelector("#root");
+
+    const h1 = document.createElement("h1");
+    h1.innerHTML = "Hello world";
+    root.appendChild(h1);
+});
